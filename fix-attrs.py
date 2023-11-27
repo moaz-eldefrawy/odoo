8024767e89dec5291a452200322f74d611dbbd8b
Moaz El-defrawy <moaz.eldefrawy@gmail.com>
2023-11-27 15:44:02 +0200

    [REN]: fix-attrs instead script.py and add fixes

--- fix-attrs.py
+++ fix-attrs.py
@@ -1,122 +1,111 @@
 import subprocess
 from os.path import expanduser
 import sys
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 HOME = expanduser("~")
-# this is the commit hash of the odoo repo that we want to use during installation
-ODOO_COMMIT_HASH = "0d99f4b9d9de404fea44ecf5480ec3b07c2b0fd6"
-PATCH_PATH = "./convert.patch"
 DB_NAME = "convert-attrs-and-states"
 ODOO_HOME = HOME + "/odoo/dev/odoo"
-
-
-addons_path = "addons/,../psbe-athem/"
-addon_install = "base,athem" 
+REMOTE_BRANCH = "https://github.com/moaz-eldefrawy/odoo"
 
 def git_stash_dir(path):
   """runs git stash on a directory and return false if there are no changes to stash"""
-  print('git stash ' + path)
+  logging.info('git stash ' + path)
   return subprocess.check_output(['git', 'stash'], cwd=path).decode("utf-8").rstrip() != "No local changes to save"
 
 def git_checkout_commit_hash(path, commit_hash):
   """runs git checkout on a directory"""
-  print('git checkout ' + commit_hash + ' ' + path)
+  logging.info('git checkout ' + commit_hash + ' ' + path)
   subprocess.call(['git', 'checkout', commit_hash], cwd=path)
 
 
 def git_apply_patch(path, patch_path):
   """runs git apply on a directory"""
-  print('git apply ' + patch_path)
+  logging.info('git apply ' + patch_path)
   subprocess.call(['git', 'apply', patch_path], cwd=path)
 
 
 def reset_db(db_name):
   """resets a database by dropping it and recreating it"""
-  print('running: dropdb ' + db_name)
+  logging.info('running: dropdb ' + db_name)
   subprocess.call(['dropdb', db_name])
-  print('running: createdb ' + db_name)
+  logging.info('running: createdb ' + db_name)
   subprocess.call(['createdb', db_name])
 
-def run_odoo_install(path, addons_dir):
+def run_odoo_install(path, addons_paths, addons_install):
   """runs odoo install on an addons directory"""
   reset_db(DB_NAME)
-  print("running: odoo-bin -d " + DB_NAME + " --addons-path " + addons_dir + " --stop-after-init")
-  subprocess.call(['./odoo-bin', '-d', DB_NAME, '--addons-path', addons_dir, '-i base,athem', '--stop-after-init'], cwd=path)
-
+  logging.info('running: odoo -i ' + addons_install + ' --addons-path=' + addons_paths + ' -d ' + DB_NAME)
+  subprocess.call(['./odoo-bin', '-i', addons_install, '--addons-path=' + addons_paths, '-d', DB_NAME, "--stop-after-init"], cwd=path)
+  
 def git_get_current_branch(path):
   """returns the current branch of a directory"""
-  print('running: git rev-parse --abbrev-ref HEAD')
+  logging.info('running: git rev-parse --abbrev-ref HEAD')
   return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], cwd=path).decode("utf-8").rstrip()
 
 def git_checkout_branch(path, branch):
   """runs git checkout on a directory"""
-  print('git checkout ' + branch)
+  logging.info('git checkout ' + branch)
   subprocess.call(['git', 'checkout', branch], cwd=path)
 
 def git_stash_pop(path):
   """runs git stash pop on a directory"""
-  print('git stash pop')
+  logging.info('git stash pop')
   subprocess.call(['git', 'stash', 'pop'], cwd=path)
 
 def git_clear_changes(path):
   """runs git reset --hard on a directory"""
-  print('git reset --hard')
+  logging.info('git reset --hard')
   subprocess.call(['git', 'reset', '--hard'], cwd=path)
 
 
 def git_add_remote(path, remote_name, remote_url):
   """runs git remote add on a directory"""
-  print('git remote add ' + remote_name + ' ' + remote_url)
+  logging.info('git remote add ' + remote_name + ' ' + remote_url)
   subprocess.call(['git', 'remote', 'add', remote_name, remote_url], cwd=path)
 
 def git_fetch_remote(path, remote_name):
   """runs git fetch on a directory"""
-  print('git fetch ' + remote_name)
+  logging.info('git fetch ' + remote_name)
   subprocess.call(['git', 'fetch', remote_name], cwd=path)
 
-def git_checkout_remote_branch(path, remote_name, branch):
-  """runs git checkout on a directory"""
-  print('git checkout ' + remote_name + '/' + branch)
-  subprocess.call(['git', 'checkout', remote_name + '/' + branch], cwd=path)
 
 def main():
+  if len(sys.argv) > 1:
+    addons_path = sys.argv[1]
+  if len(sys.argv) > 2:
+    addons_install = sys.argv[2]
+  
+  if not addons_path and not addons_install:
+    logging.error("addons_path or addons_install are not provided")
+    sys.exit(1)
 
-  script_args = sys.argv[1:]
-  addons_path = script_args[0]
+  logging.info("starting: fix `attrs` and `states` fields")
 
-  # git add moaz-origin https://github.com/moaz-eldefrawy/odoo
-  # git fetch moaz-origin
-  print("fixing `attrs` and `states` fields in odoo")
-
-  # get the current branch
   current_branch = git_get_current_branch(ODOO_HOME)
-  print('current branch: ' + current_branch)
+  logging.info('current branch: ' + current_branch)
 
-  # stash the current branch
   changes_staged: bool = git_stash_dir(ODOO_HOME)
-  
-  # # checkout the commit hash
-  # git_checkout_commit_hash(ODOO_HOME, ODOO_COMMIT_HASH)
 
-  # #  apply the patch
-  # git_apply_patch(ODOO_HOME, PATCH_PATH)
-
-  git_add_remote(ODOO_HOME, 'moaz-origin', 'https://github.com/moaz-eldefrawy/odoo')
+  # checkout the branch that has the fix
+  git_add_remote(ODOO_HOME, 'moaz-origin', REMOTE_BRANCH)
   git_fetch_remote(ODOO_HOME, 'moaz-origin')
-  git_checkout_remote_branch(ODOO_HOME, 'moaz-origin', 'convert-attrs-and-states')
-  
-  # run odoo install
-  run_odoo_install(ODOO_HOME, addons_path)
+  git_checkout_branch(ODOO_HOME, 'moaz-origin/17.0-fix_attrs_and_states-meld')
 
-  # clear changes
+  run_odoo_install(ODOO_HOME, addons_path, addons_install)
+
   git_clear_changes(ODOO_HOME)
 
-  #  checkout the current branch
   git_checkout_branch(ODOO_HOME, current_branch)
 
-  # stash pop
   if changes_staged:
     git_stash_pop(path=ODOO_HOME)
 
 
 main()
+
+
+# example: python3 fix-attrs.py addons/,../psbe-mintjens/ mintjens_sale_stock
+
